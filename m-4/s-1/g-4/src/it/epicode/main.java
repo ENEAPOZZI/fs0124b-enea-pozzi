208e32e2b734865516ebb45f15abc223e52ecad7
EneaPozzi <eppsm2001@gmail.com>
2024-04-19 16:05:33 +0200

    verifica

--- m-4/s-1/g-4/src/it/epicode/main.java
+++ m-4/s-1/g-4/src/it/epicode/main.java
@@ -1,4 +1,15 @@
 package it.epicode;
 
 public class main {
+
+    private BloccoMotore motore;
+
+    public main{int cilindrata}{
+
+        this.motore = new BloccoMotore(cilindrata);
+    }
+
+
+
+
 }
